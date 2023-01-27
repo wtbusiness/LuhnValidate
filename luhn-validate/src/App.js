5209bdb1b49b095c2252cc840dec0cf488eb977d
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-01-27 14:00:21 -0500

    LuhnValidate

--- luhn-validate/src/App.js
+++ luhn-validate/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
-  function validateLuhn(cardNumber) {
-    console.log(typeof cardNumber);
+   const [updated, setUpdated] = useState("");
+  // const [isValid, setIsValid] = useState(null);
+
+  // useEffect
+  useEffect(() => {
+ validateLuhn()
+  
+  },[updated])
+
+  function validateLuhn() {
+    // console.log("number",typeof cardNumber);
     //let reversedCardNumber = cardNumber.split("");
     let total = 0;
-    let myArr = String(cardNumber)
+    let myArr = String(updated)
       .split("")
-      .map((cardNumber) => {
-        return Number(cardNumber);
+      .map((updated) => {
+        return Number(updated);
       }).reverse();
     console.log(myArr);
     for (let i = 0; i < myArr.length; i++) {
@@ -24,24 +33,36 @@ const App = () => {
       total += currentNumber;
     }
       if (total % 10 === 0) {
-    return true;
+        console.log(true)
+    //return true;
   } else {
-    return false;
+    console.log(false);
+    //return false;
   }
   } 
   
-  console.log(validateLuhn(1234567890123456));
+ 
+ // console.log(validateLuhn(1234567890123456));
 
-  console.log(validateLuhn(4408041234567893));
+  // console.log(validateLuhn(4408041234567893));
 
-  console.log(validateLuhn(38520000023237));
+  // console.log(validateLuhn(38520000023237));
 
-  console.log(validateLuhn(4222222222222));
+  // console.log(validateLuhn(4222222222222));
+
+
   return (
     <div>
-      
+      <label>
+        Credit Card:
+        <input type="text" name="credit-card" onChange={(e) => setUpdated(e.target.value)} value={updated} />
+      </label>
+      {/* <input onClick={validateLuhn(updated)} type="submit" value="Submit" /> */}
+
+      {/* {isValid === true ? <p>Card number is valid</p> : null}
+      {isValid === false ? <p>Card number is not valid</p> : null} */}
     </div>
-  )
+  );
 }
 
 export default App
